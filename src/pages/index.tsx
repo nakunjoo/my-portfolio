8d50d582f85c9d1b1e9fb8651977acca2b97c624
nkj960822 <48468694+nakunjoo@users.noreply.github.com>
2024-05-21 23:54:13 +0900

    05.21

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,45 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
 export default function MainPage() {
   return (
     <div className={'mainWrap'}>
-      <h1>{'Main Page'}</h1>
-      <div>{'asd'}</div>
+      <div className={'bannerWrap'}>
+        <div className={'bannerBg'} />
+        <h1>{'PortFolio'}</h1>
+      </div>
+      <div className={'aboutWrap'}>
+        <Container>
+          <Row>
+            <Col xl={4} gl={12} className={'title'}>
+              <span>{'About Me'}</span>
+            </Col>
+            <Col xl={8} gl={12} className={'infoBox'}>
+              <Row>
+                <Col xl={4} gl={12}>
+                  <span className={'subTitle'}>{'Name'}</span>
+                  <span>{'나군주'}</span>
+                </Col>
+                <Col xl={4} gl={12}>
+                  <span className={'subTitle'}>{'Birth'}</span>
+                  <span>{'1996.08.22'}</span>
+                </Col>
+                <Col xl={4} gl={12}>
+                  <span className={'subTitle'}>{'Address'}</span>
+                  <span>{'서울시 은평구'}</span>
+                </Col>
+                <Col xl={4} gl={12}>
+                  <span className={'subTitle'}>{'Phone'}</span>
+                  <span>{'010.4147.7756'}</span>
+                </Col>
+                <Col xl={4} gl={12}>
+                  <span className={'subTitle'}>{'Email'}</span>
+                  <span>{'nkj960822@gmail.com'}</span>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
